d2eb733482aecd9c90b25dcf40fd2fcc52b781f0
Silas Morales <silas.mora22@gmail.com>
2025-02-20 23:38:01 -0800

    postgres is set up, setting up prisma

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Instrument_Sans } from "next/font/google";
 import "./globals.css";
+import SessionProviderWrapper from "./components/SessionProviderWrapper";
 
 const instrumentSans = Instrument_Sans({
   variable: "--font-instrument-sans",
@@ -20,12 +21,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${instrumentSans.variable} box-border m-0 p-0`}
-      >
+    <SessionProviderWrapper>
+      <html lang="en">
+        <body
+          className={`${instrumentSans.variable} box-border m-0 p-0`}
+        >
         {children}
-      </body>
-    </html>
+        </body>
+      </html>
+   </SessionProviderWrapper> 
   );
 }
